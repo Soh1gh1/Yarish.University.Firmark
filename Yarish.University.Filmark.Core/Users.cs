f62d8c8739347cb1b18ccb456a0344b0ca2fc0f2
Soh1gh1 <Soh1gh3105@gmail.com>
2023-05-13 01:21:21 +0300

    New features

---
+++ Yarish.University.Filmark.Core/Users.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yarish.University.Filmark.Core
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public DateTime RegistrationDate { get; set; }
+
+		public class Role
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+		}
+		public class UserRole
+		{
+			public int UserId { get; set; }
+			public int RoleId { get; set; }
+		}
+
+	}
+
+}
